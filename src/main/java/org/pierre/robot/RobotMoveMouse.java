565f762e5edcbfb616fa0a3425498e2f12ef6077
vernetto <Ernesto0z>
2020-10-19 14:28:12 +0200

    add date

--- src/main/java/org/pierre/robot/RobotMoveMouse.java
+++ src/main/java/org/pierre/robot/RobotMoveMouse.java
@@ -2,6 +2,7 @@ package org.pierre.robot;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.Date;
 
 public class RobotMoveMouse {
     public static void main(String[] args) throws AWTException {
@@ -13,6 +14,7 @@ public class RobotMoveMouse {
             robot.delay(60000);
             robot.delay(60000);
             robot.mouseWheel(-1);
+            System.out.println(new Date());
         }
     }
 }
