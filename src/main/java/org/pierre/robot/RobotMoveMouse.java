e877423f90aa5f3c5317a6f88382637f4c093b44
vernetto <Ernesto0z>
2021-01-29 08:53:45 +0100

    add date

--- src/main/java/org/pierre/robot/RobotMoveMouse.java
+++ src/main/java/org/pierre/robot/RobotMoveMouse.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class RobotMoveMouse {
 
     public static final int MS = 60000;
-    public static final int NUMBEROFLOOPS = 5;
+    public static final int NUMBEROFLOOPS = 2;
     public static final int LOOPS = 2000000;
     public static Robot robot ;
 
@@ -16,15 +16,17 @@ public class RobotMoveMouse {
         robot = new Robot();
         for (int i = 0; i < LOOPS; i++) {
             waitFor(NUMBEROFLOOPS);
-            PointerInfo pi = MouseInfo.getPointerInfo();
             //System.out.println(pi.getDevice().getIDstring());
             //System.out.println(pi.getLocation());
-            if ("\\Display0".equals(pi.getDevice().getIDstring())) {
+            if ("\\Display0".equals(MouseInfo.getPointerInfo().getDevice().getIDstring())) {
                 java.awt.Toolkit.getDefaultToolkit().beep();
             }
             robot.mouseWheel(1);
             waitFor(NUMBEROFLOOPS);
             robot.mouseWheel(-1);
+            if ("\\Display0".equals(MouseInfo.getPointerInfo().getDevice().getIDstring())) {
+                java.awt.Toolkit.getDefaultToolkit().beep();
+            }
             System.out.println(new Date());
         }
     }
