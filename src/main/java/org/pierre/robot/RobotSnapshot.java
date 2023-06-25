618a6d3ad2da05568933f354032f124f2b10e3a6
vernetto <Ernesto0z>
2023-06-25 15:14:13 +0200

    pgdown

--- src/main/java/org/pierre/robot/RobotSnapshot.java
+++ src/main/java/org/pierre/robot/RobotSnapshot.java
@@ -9,17 +9,30 @@ public class RobotSnapshot {
         Robot robot = new Robot();
         for (int i = 0; i < 2000; i++) {
             robot.delay(2000);
-            robot.keyPress(KeyEvent.VK_RIGHT);
-            robot.keyRelease(KeyEvent.VK_RIGHT);
+            moveWithPageDown(robot);
             robot.delay(2000);
             System.out.println(new Date() + " printscreen");
             //KeyEvent keyEvent = new KeyEvent();
-            robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_PRINTSCREEN);
-            robot.keyRelease(KeyEvent.VK_PRINTSCREEN);
-            robot.keyRelease(KeyEvent.VK_ALT);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
+            takeSnapshot(robot);
         }
+    }
+
+    private static void takeSnapshot(Robot robot) {
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ALT);
+        robot.keyPress(KeyEvent.VK_PRINTSCREEN);
+        robot.keyRelease(KeyEvent.VK_PRINTSCREEN);
+        robot.keyRelease(KeyEvent.VK_ALT);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+    }
+
+    private static void moveWithLeftArrow(Robot robot) {
+        robot.keyPress(KeyEvent.VK_RIGHT);
+        robot.keyRelease(KeyEvent.VK_RIGHT);
+    }
+
+    private static void moveWithPageDown(Robot robot) {
+        robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+        robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
     }
 }
